d13aa382b83d2947439f0d6705fc2c7cbe245b68
rushilpatel22 <50056447+rushilpatel22@users.noreply.github.com>
2020-08-13 14:47:09 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args){
+        // Hello World test program
+        System.out.println("Hello World"); 
+        // All about me
+        System.out.println("My name is Rushil and I want to major in Computer Science."); 
+        System.out.println("I recently participated in a hackathon this summer."); 
+        System.out.println("I am part of ASB and a team captain for our school's cross country team!"); 
+        System.out.println("I would like to go to UC Berkeley."); 
+        System.out.println("My favorite video game is Geometry Dash."); 
+    }
+}
